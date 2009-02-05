161b53f3cd6cd7257f1bed9a785f8abb540b609a
Leandro Perrotta <leandro_perrotta@hotmail.com>
2009-02-05 20:44:27 +0000

    *fix :P
    git-svn-id: svn://darghos.com.br/darghos/website/new2.0@555 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- modules/account/advanced_recovery.php
+++ modules/account/advanced_recovery.php
@@ -12,103 +12,116 @@ if($_SESSION['recovery'])
 	$account->load($character->get("account_id"), "password, email");
 	$secretkey = $account->getSecretKey();
 	
-	if($post)
-	{	
-		if($core->getIpTries() >= 3)
-		{
-			$error = "Está operação está bloqueada, por favor aguarde 24 horas após a ultima tentativa.";
-		}
-		elseif($post[2] != $secretkey['key'])
-		{
-			$core->increaseIpTries();
-			
-			if($core->getIpTries() < 3)
-				$error = "A chave secreta informada para mudança de e-mail de sua conta está incorreta. Por movitos de segurança você só poderá efetuar 3 tentativas desta operação, após as 3 tentativas este recurso estará bloqueado por 24 horas.";
+	if($secretkey)
+	{
+		if($post)
+		{	
+			if($core->getIpTries() >= 3)
+			{
+				$error = "Está operação está bloqueada, por favor aguarde 24 horas após a ultima tentativa.";
+			}
+			elseif($post[2] != $secretkey['key'])
+			{
+				$core->increaseIpTries();
+				
+				if($core->getIpTries() < 3)
+					$error = "A chave secreta informada para mudança de e-mail de sua conta está incorreta. Por movitos de segurança você só poderá efetuar 3 tentativas desta operação, após as 3 tentativas este recurso estará bloqueado por 24 horas.";
+				else
+					$error = "Você efetuou três tentativas erradas desta operação, por motivos de segurança este recurso estará bloqueado pelas proximas 24 horas.";
+			}
+			elseif($chkEmail->loadByEmail($post[3]))
+			{
+				$error = "Este e-mail já esta em uso por outra conta em nosso banco de dados.";
+			}
+			elseif(!$strings->validEmail($post[3]))
+			{
+				$error = "Este não é um e-mail valido.";
+			}
 			else
-				$error = "Você efetuou três tentativas erradas desta operação, por motivos de segurança este recurso estará bloqueado pelas proximas 24 horas.";
+			{
+				$account->set("email", $post[3]);
+				$account->save();
+				
+				$success = "
+				<p>Caro jogador,</p>
+				<p>O e-mail registrado em sua conta foi modificado para {$post[3]} ultilizando sua chave secreta com sucesso!</p>
+				<p>Tenha um bom jogo!</p>
+				";		
+			}
 		}
-		elseif($chkEmail->loadByEmail($post[3]))
-		{
-			$error = "Este e-mail já esta em uso por outra conta em nosso banco de dados.";
-		}
-		elseif(!$strings->validEmail($post[3]))
-		{
-			$error = "Este não é um e-mail valido.";
-		}
-		else
-		{
-			$account->set("email", $post[3]);
-			$account->save();
-			
-			$success = "
-			<p>Caro jogador,</p>
-			<p>O e-mail registrado em sua conta foi modificado para {$post[3]} ultilizando sua chave secreta com sucesso!</p>
-			<p>Tenha um bom jogo!</p>
-			";		
-		}
-	}
 	
-	if($success)	
-	{
-		$module .=	'
-			
-		<div id="sucesso">
-			<h2>'.$success.'</h2>
-		</div>
-		
-		';
-	}
-	else
-	{
-		if($error)	
+		if($success)	
 		{
 			$module .=	'
-			
-			<div id="error">
-				<h2>'.$error.'</h2>
+				
+			<div id="sucesso">
+				<h2>'.$success.'</h2>
 			</div>
 			
 			';
 		}
-		
-	$module .= '	
-		<form action="" method="post">
-			<fieldset>			
-		
-				<p>
-					<label for="recovery_name">Nome do Personagem</label><br />
-					<input readonly="readonly" name="recovery_name" size="40" type="text" value="'.$_SESSION['recovery'][0].'" />
-				</p>		
+		else
+		{
+			if($error)	
+			{
+				$module .=	'
+				
+				<div id="error">
+					<h2>'.$error.'</h2>
+				</div>
 				
 				';
-		
-				if($secretkey["lembrete"] != "default")
-				{
+			}
+			
+		$module .= '	
+			<form action="" method="post">
+				<fieldset>			
+			
+					<p>
+						<label for="recovery_name">Nome do Personagem</label><br />
+						<input readonly="readonly" name="recovery_name" size="40" type="text" value="'.$_SESSION['recovery'][0].'" />
+					</p>		
+					
+					';
+			
+					if($secretkey["lembrete"] != "default")
+					{
+						$module .= '
+						<p>
+							<label for="recovery_character">Lembrete</label><br />
+							<input readonly="readonly" name="recovery_lembrete" size="40" type="text" value="'.$secretkey["lembrete"].'" /> <br><em>(lembrete de sua chave secreta configurado por você)</em>
+						</p>';			
+					}			
+					
 					$module .= '
 					<p>
-						<label for="recovery_character">Lembrete</label><br />
-						<input readonly="readonly" name="recovery_lembrete" size="40" type="text" value="'.$secretkey["lembrete"].'" /> <br><em>(lembrete de sua chave secreta configurado por você)</em>
-					</p>';			
-				}			
-				
-				$module .= '
-				<p>
-					<label for="recovery_secretkey">Chave Secreta</label><br />
-					<input name="recovery_secretkey" size="40" type="password" value="" /> <br><em>(informe sua chave secreta corretamente)</em>
-				</p>
-				
-				<p>
-					<label for="recovery_email">Novo e-mail</label><br />
-					<input name="recovery_email" size="40" type="text" value="" /> <br><em>(informe o novo endereço de e-mail que você deseja configurar em sua conta)</em>
-				</p>			
-				
-				<div id="line1"></div>				
-				
-				<p>
-					<input type="submit" value="Proximo" />					
-				</p>
-		</fieldset>
-	</form>';
+						<label for="recovery_secretkey">Chave Secreta</label><br />
+						<input name="recovery_secretkey" size="40" type="password" value="" /> <br><em>(informe sua chave secreta corretamente)</em>
+					</p>
+					
+					<p>
+						<label for="recovery_email">Novo e-mail</label><br />
+						<input name="recovery_email" size="40" type="text" value="" /> <br><em>(informe o novo endereço de e-mail que você deseja configurar em sua conta)</em>
+					</p>			
+					
+					<div id="line1"></div>				
+					
+					<p>
+						<input type="submit" value="Proximo" />					
+					</p>
+			</fieldset>
+		</form>';
+		}
 	}
+	else
+	{
+		$module .=	'
+		
+		<div id="error">
+			<h2>Esta conta não possui uma chave secreta configurada.</h2>
+		</div>
+		
+		';	
+	}		
 }
 ?>
