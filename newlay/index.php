2f2abe5c7a62d899377447700be806dbd72f6110
Leandro Perrotta <leandro_perrotta@hotmail.com>
2009-10-05 14:21:35 +0000

    *adicionado banner AdSense
    git-svn-id: svn://darghos.com.br/darghos/website/branch/0.2@1197 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- newlay/index.php
+++ newlay/index.php
@@ -242,6 +242,20 @@
 						<?php } ?>
 						
 						<div>
+							<script type="text/javascript"><!--
+							google_ad_client = "pub-1678394806564868";
+							/* 468x60, criado 05/10/09 */
+							google_ad_slot = "9820027335";
+							google_ad_width = 468;
+							google_ad_height = 60;
+							//-->
+							</script>
+							<script type="text/javascript"
+							src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+							</script>			
+						</div>	
+						
+						<div>
 							<? echo $module; ?>
 						</div>
 					</div>
