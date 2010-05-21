af89a7e01f57f8c08905f0d155b8d45abce56f36
Leandro Perrotta <leandro_perrotta@hotmail.com>
2010-05-21 16:03:55 +0000

    *Corrigido bug na função Guilds->OnWar() que não estava verificando na lista de oponentes.
    git-svn-id: svn://darghos.com.br/darghos/website/trunk@1538 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- classes/Guilds.php
+++ classes/Guilds.php
@@ -799,7 +799,7 @@ class Guilds
 	
 	function OnWar()
 	{
-		$query = Core::$DB->query("SELECT `id` FROM `guild_wars` WHERE `guild_id` = '{$this->_id}' AND `status` = '".GUILD_WAR_STARTED."'");
+		$query = Core::$DB->query("SELECT `id` FROM `guild_wars` WHERE (`guild_id` = '{$this->_id}' OR `opponent_id` = '{$this->_id}') AND `status` = '".GUILD_WAR_STARTED."'");
 		return (($query->numRows() != 0) ? true : false);
 	}
 	
