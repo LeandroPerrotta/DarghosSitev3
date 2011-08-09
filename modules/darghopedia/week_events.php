2e130e1ab6034abdb7ac57c54b21e6d4529a586a
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-08-09 11:05:04 +0000

    git-svn-id: svn://darghos.com.br/darghos/website/trunk@2273 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ modules/darghopedia/week_events.php
@@ -0,0 +1,40 @@
+<?php
+$pageDb = new PagesDB();
+
+$pageDb->load(DBPAGES_DARGHOPEDIA_PVP_ARENAS);
+
+if(!$_SESSION['login'])
+{
+	$pageDb->printContent();
+}
+if($_SESSION['login'])
+{	
+	$account = new Account();
+	$account->load($_SESSION['login'][0]);
+	
+	if($account->getGroup() >= GROUP_COMMUNITYMANAGER)
+	{
+		if($_POST)
+		{
+			$pageDb->setContent($_POST['Page']);
+			$pageDb->save(DBPAGES_DARGHOPEDIA_PVP_ARENAS);
+			
+			Core::sendMessageBox("Sucesso!", "A pagina foi editada com sucesso!");
+			$pageDb->load(DBPAGES_DARGHOPEDIA_PVP_ARENAS);
+		}		
+		
+		$module .= "<br><form action='{$_SERVER['REQUEST_URI']}' method='post'>";
+		
+		$FCKEditor = Core::FCKEditor("Page");
+		
+		$FCKEditor->BasePath = "libs/fckeditor/";
+		$FCKEditor->Height = "800";
+		$FCKEditor->Value = $pageDb->getContent();
+		$module .= $FCKEditor->CreateHtml();
+		
+		$module .= "</form>";
+	}
+	else
+		$pageDb->printContent();
+}
+?>
