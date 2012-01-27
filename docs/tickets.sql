18cf9d155ba0b27846e5a347b831130ebfee1d89
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-01-27 05:56:48 +0000

    * mais uma parte...
    git-svn-id: svn://darghos.com.br/darghos/website/trunk@3028 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ docs/tickets.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `darghos`.`wb_tickets` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `player_id` INTEGER UNSIGNED NOT NULL,
+  `account` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `question` VARCHAR(255) NOT NULL,
+  `send_date` VARCHAR(45) NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  `closed` VARCHAR(45) NOT NULL, 
+  `last_update` VARCHAR(45) NOT NULL,
+  `fixed` INTEGER UNSIGNED NOT NULL,
+  `attachment` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE `darghos`.`wb_tickets_answers` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ticket_id` VARCHAR(45) NOT NULL,
+  `text` TEXT NOT NULL,
+  `by_name` VARCHAR(45) NOT NULL,
+  `send_date` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
+test
+
+ALTER TABLE `darghos`.`wb_tickets` ADD COLUMN `fixed` INTEGER UNSIGNED NOT NULL AFTER `last_update`;
+ALTER TABLE `darghos`.`wb_tickets_answers` MODIFY COLUMN `text` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+, ROW_FORMAT = DYNAMIC;
---
+++ language/pt-br/pages.php
@@ -0,0 +1,36 @@
+<?php
+global $pages;
+
+$pages["ACCOUNT.REGISTER.EMAIL_ADDRESS"] = "Endereço de E-mail"; 
+$pages["ACCOUNT.REGISTER.ACCOUNT_NAME"] = "Nome da Conta"; 
+$pages["ACCOUNT.REGISTER.PRIVACY_POLICY_ACCEPT"] = "Eu concordo com a politica de privacidade deste site."; 
+
+$pages["ACCOUNT.LOGIN.ACCOUNT_NAME"] = "Nome da Conta"; 
+$pages["ACCOUNT.LOGIN.PASSWORD"] = "Senha"; 
+
+$pages["ACCOUNT.RECOVERY.CHARACTER_NAME"] = "Nome do Personagem"; 
+$pages["ACCOUNT.RECOVERY.CHARACTER_NAME_INFO"] = "(preencha com o nome de um personagem da conta em que você quer recuperar)"; 
+$pages["ACCOUNT.RECOVERY.EMAIL"] = "E-mail da Conta"; 
+$pages["ACCOUNT.RECOVERY.EMAIL_INFO"] = "(preencha com o e-mail registrado na conta de seu personagem)"; 
+$pages["ACCOUNT.RECOVERY.DATA_TO_RECOVERY"] = "Dado a se Recuperar"; 
+$pages["ACCOUNT.RECOVERY.RECOVERY_ACCOUNT_NAME"] = "Eu quero receber o nome de minha conta em meu e-mail."; 
+$pages["ACCOUNT.RECOVERY.RECOVERY_PASSWORD"] = "Eu quero receber uma nova senha para minha conta em meu e-mail."; 
+$pages["ACCOUNT.RECOVERY.RECOVERY_BOTH"] = "Eu quero receber o nome e uma nova senha para minha conta em meu e-mail."; 
+$pages["ACCOUNT.RECOVERY.ADVANCED_RECOVERY"] = "Recuperação Avançada com minha Chave Secreta"; 
+$pages["ACCOUNT.RECOVERY.USE_ADVANCED_RECOVERY"] = "Eu não consegui recuperar minha conta com as informações acima e desejo recupera-la usando a minha chave secreta."; 
+
+$pages["ACCOUNT.ADVANCED_RECOVERY.CHARACTER_NAME"] = "Nome do Personagem"; 
+$pages["ACCOUNT.ADVANCED_RECOVERY.REMINDER"] = "Lembrete"; 
+$pages["ACCOUNT.ADVANCED_RECOVERY.REMINDER_DESC"] = "lembrete de sua chave secreta configurado por você"; 
+$pages["ACCOUNT.ADVANCED_RECOVERY.SECRETKEY"] = "Chave Secreta"; 
+$pages["ACCOUNT.ADVANCED_RECOVERY.SECRETKEY_DESC"] = "informe sua chave secreta corretamente"; 
+$pages["ACCOUNT.ADVANCED_RECOVERY.NEW_EMAIL"] = "Novo e-mail";
+$pages["ACCOUNT.ADVANCED_RECOVERY.NEW_EMAIL_DESC"] = "informe o novo endereço de e-mail que você deseja configurar em sua conta";
+
+$pages["ACCOUNT.CHANGE_PASSWORD.NEW_PASSWORD"] = "Nova Senha"; 
+$pages["ACCOUNT.CHANGE_PASSWORD.NEW_PASSWORD_CONFIRM"] = "Confirmar nova Senha"; 
+$pages["ACCOUNT.CHANGE_PASSWORD.CURRENT_PASSWORD"] = "Senha Atual"; 
+
+$pages["ACCOUNT.CHANGE_EMAIL.NEW_EMAIL"] = "Novo e-mail"; 
+$pages["ACCOUNT.CHANGE_EMAIL.PASSWORD"] = "Senha"; 
+?>
