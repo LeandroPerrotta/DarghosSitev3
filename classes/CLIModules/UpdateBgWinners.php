567fef02b8643702b67fc271b319856d7aecfd76
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-05-25 07:28:12 +0000

    git-svn-id: svn://darghos.com.br/darghos/website/trunk@3434 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- classes/CLIModules/UpdateBgWinners.php
+++ classes/CLIModules/UpdateBgWinners.php
@@ -17,10 +17,10 @@ class UpdateBgWinners
 	function Run()
 	{
 		$query = $this->db->query("SELECT `player_id`, COUNT(*) as `wins` FROM `player_history` WHERE `type` = 1 AND `history` = '1' AND `date` >= UNIX_TIMESTAMP() - (60 * 60 * 24 * 7) GROUP BY `player_id` ORDER BY `wins` DESC");
-		$winners = $query->fetchArray();
+		$winners = $query->fetchAssocArray();
 		
 		$query = $this->db->query("SELECT `player_id`, COUNT(*) as `losses` FROM `player_history` WHERE `type` = 1 AND `history` = '2' AND `date` >= UNIX_TIMESTAMP() - (60 * 60 * 24 * 7) GROUP BY `player_id` ORDER BY `losses` DESC");
-		$losses = $query->fetchArray();
+		$losses = $query->fetchAssocArray();
 		
 		$array = array_merge_recursive($winners, $losses);
 		$json = json_encode($array);
