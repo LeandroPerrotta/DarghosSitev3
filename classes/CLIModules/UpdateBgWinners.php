aaeb0c55c38ac351d8708c0ae60e64ba4321f05e
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-05-25 08:06:44 +0000

    git-svn-id: svn://darghos.com.br/darghos/website/trunk@3437 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- classes/CLIModules/UpdateBgWinners.php
+++ classes/CLIModules/UpdateBgWinners.php
@@ -18,14 +18,14 @@ class UpdateBgWinners
 	{
 		$query = $this->db->query("SELECT `player_id`, COUNT(*) as `wins` FROM `player_history` WHERE `type` = 1 AND `history` = '1' AND `date` >= UNIX_TIMESTAMP() - (60 * 60 * 24 * 7) GROUP BY `player_id` ORDER BY `wins` DESC");
 		
-		$wins = array();
+		$data = array();
 		
 		if($query->numRows() == 0)
 			return;
 		
 		while(($fetch = $query->fetch()))
 		{
-			array_push($wins, array("player_id" => $fetch->player_id, "wins" => $fetch->wins));
+			array_push($data, array("player_id" => $fetch->player_id, "wins" => $fetch->wins));
 		}
 		
 		$losses = array();
@@ -35,13 +35,19 @@ class UpdateBgWinners
 		if($query->numRows() > 0)
 		{
 			while(($fetch = $query->fetch()))
-			{
-				array_push($losses, array("player_id" => $fetch->player_id, "losses" => $fetch->losses));	
+			{	
+				foreach($data as $k => $d)
+				{
+					if($d["player_id"] == $fetch->player_id)
+					{
+						$data[$k]["losses"] = $fetch->losses;
+						break;
+					}
+				}
 			}
 		}
 		
-		$array = array_merge_recursive($wins, $losses);
-		$json = json_encode($array);
+		$json = json_encode($data);
 		
 		\Core\Main::writeTempFile("bgbest.json", $json);
 	}
