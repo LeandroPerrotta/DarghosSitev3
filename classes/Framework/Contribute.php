3f083c1aa233fe09a0677b9a02ceff2046d3c5b2
Leandro <leandro_perrotta@hotmail.com>
2013-03-12 16:40:23 -0300

    * Ajustado preço das premiums.

--- classes/Framework/Contribute.php
+++ classes/Framework/Contribute.php
@@ -4,11 +4,11 @@ use \Core\Consts;
 class Contribute extends \Core\MySQL
 {		
 	static public $premiums = array(
-		array("period" => 10, "product" => "Contribuicao para 10 dias de Conta Premium", "text" => "10 dias.", "cost" => 4.50),
-		array("period" => 30, "product" => "Contribuicao para 30 dias de Conta Premium", "text" => "30 dias.", "cost" => 11.90),
-		array("period" => 60, "product" => "Contribuicao para 60 dias de Conta Premium", "text" => "60 dias (2 meses).", "cost" => 22.50),
-		array("period" => 90, "product" => "Contribuicao para 90 dias de Conta Premium", "text" => "90 dias (3 meses).", "cost" => 31.90),
-		array("period" => 180, "product" => "Contribuicao para 180 dias de Conta Premium", "text" => "180 dias (6 meses).", "cost" => 59.90)
+		array("period" => 10, "product" => "Contribuicao para 10 dias de Conta Premium", "text" => "10 dias.", "cost" => 3.50),
+		array("period" => 30, "product" => "Contribuicao para 30 dias de Conta Premium", "text" => "30 dias.", "cost" => 9.00),
+		array("period" => 60, "product" => "Contribuicao para 60 dias de Conta Premium", "text" => "60 dias (2 meses).", "cost" => 17.00),
+		array("period" => 90, "product" => "Contribuicao para 90 dias de Conta Premium", "text" => "90 dias (3 meses).", "cost" => 24.50),
+		array("period" => 180, "product" => "Contribuicao para 180 dias de Conta Premium", "text" => "180 dias (6 meses).", "cost" => 45.00)
 	);
 	
 	static public $premiumsPromotions = array(
