dcb5a0c1f0a44495b7b69d2d33f1ae501ca78f53
Leandro <leandro_perrotta@hotmail.com>
2013-08-16 01:15:43 -0300

    * forgot to commit

---
+++ modules/adv/monsterfactor.php
@@ -0,0 +1,99 @@
+<?php
+$monster = \Framework\Monsters::GetInstance();
+$monster instanceof \Framework\Monsters;
+
+$it = $monster->getList()->getIterator();
+$it instanceof ArrayIterator;
+
+global $module;
+
+$dataPatch = \Core\Configs::Get(\Core\Configs::eConf()->PATCH_SERVER) . \Core\Configs::Get(\Core\Configs::eConf()->FOLDER_DATA);
+$respawns_patch = $dataPatch . "world/-spawn.xml";
+$spawns_list = array();
+$total_spawns = 0;
+
+if(file_exists($respawns_patch)){
+   $spawns = new \DOMDocument();
+   
+   $spawns->load($respawns_patch);
+   
+   $list = $spawns->getElementsByTagName("monster");
+   for($x = 0; $x < $list->length; $x++)
+   {
+       $name = $list->item($x)->getAttribute("name");     
+       $spawns_list[strtolower($name)]++;
+       $total_spawns++;
+   }
+}
+
+$table = "";
+
+$list = array();
+
+while($it->valid())
+{
+	$info = $it->current();
+	$name = $it->key();
+	
+	if(isset($spawns_list[strtolower($name)])){
+    	$monster->loadByName($name);
+    	
+    	if($monster->getExperience() > 600){
+    
+        	$spawns_count = $spawns_list[strtolower($name)];
+        	$powerFactor = number_format($monster->getExperience() / $monster->getHealthMax(), 2);
+        	$attackFactor = number_format($monster->getExperience() / abs($monster->getMaxDamage()), 2);
+        	
+        	$data = array(
+        	    "name" => $name
+                ,"factor" => $powerFactor . "/" . $attackFactor
+                ,"exp" => $monster->getExperience()
+                ,"life" => $monster->getHealthMax()   
+                ,"count" => $spawns_count   
+            );
+        	
+        	array_push($list, $data);
+    	}
+	}
+	
+	$it->next();
+}
+
+$tmp = array();
+foreach($list as &$v)
+    $tmp[] = &$v["count"];
+
+array_multisort($tmp, $list);
+
+foreach($list as &$data){
+    $table .= "
+    <tr>
+        <td><a href='?ref=darghopedia.monster&name={$data["name"]}'>{$data["name"]}</a></td>
+        <td>".$data["factor"]."</td>
+        <td>".$data["exp"]."</td>
+        <td>".$data["life"]."</td>
+        <td>".$data["count"]." (".\Core\Tools::getPercentOf($data["count"], $total_spawns, 2).")</td>
+    </tr>";
+}
+
+if($table == "")
+{
+	$table .= "
+	<tr>
+		<td colspan='3'>Selecione uma categoria acima.</td> 
+	</tr>";		
+}
+
+$module .= '	
+<table cellspacing="0" cellpadding="0" id="table">
+    <p>Total Spawns: '.$total_spawns.'</p>
+
+	<tr>
+		<th>Nome</th>
+		<th>Fator</th>
+		<th>Exp</th>
+		<th>Vida</th>
+		<th>Qtd</th>
+	</tr>	
+'.$table.'
+</table>';
---
+++ modules/adv/translations.php
@@ -0,0 +1,105 @@
+<?php
+
+if(!$_GET["lang"]){
+    
+    $select = "<select name='lang' style='width: 120px'>";
+    
+    $select .= "<option value='en'>Inglês</option>";
+    
+    $select .= "</select>";
+    
+    $module .= "
+    
+    <form action='{$_SERVER['REQUEST_URI']}' method='GET'>
+    
+        <fieldset>
+            <input type='hidden' name='ref' value='adv.translations'>
+        
+            <p>
+                <label for='lang'>Idioma</label>
+                {$select}
+            </p>
+            
+			<div id='line1'></div>
+			
+			<p>
+				<input class='button' type='submit' value='Enviar' />
+			</p>	            
+        </fieldset>
+    </form>
+    
+    ";    
+}
+else{
+    
+    $file = "language/" . $_GET["lang"] . ".json";
+    $temp = file_get_contents($file);
+    
+    if($temp){
+        
+        $data = json_decode($temp);
+        
+        if($_POST){
+            
+            $k = 0;
+            
+            foreach($data as $key => $value){
+                
+                $data->{$key} = $_POST["key_{$k}"];
+                $k++;
+            }
+            
+            $json = json_encode($data);
+            file_put_contents($file, $json);
+        }    
+        
+        $inputs = "";
+        
+        $k = 0;
+        foreach($data as $key => $value){
+            
+            $value = htmlspecialchars($value);
+            $key = htmlspecialchars($key);
+            
+            if(strlen($value) <= 128){
+                $inputs .= "
+                    <p style='margin-bottom: 25px;'>
+                        <label for='{$key}'>{$key}</label>
+                        <input type='text' name='key_{$k}' style='width: 300px;' value='{$value}'/>
+                    </p>
+                ";
+            }
+            else{
+                $inputs .= "
+                <p style='margin-bottom: 25px;'>
+                    <label for='{$key}'>{$key}</label>
+                    <textarea rows='16' cols='96' name='key_{$k}'>{$value}</textarea>
+                </p>
+                ";
+            }
+            
+            $k++;
+        }
+        
+        $module .= "
+        
+        <form action='{$_SERVER['REQUEST_URI']}' method='POST'>
+        
+            <fieldset>                
+                {$inputs}
+                
+    			<div id='line1'></div>
+    			
+    			<p>
+    				<input class='button' type='submit' value='Enviar' />
+    			</p>                 
+            </fieldset>           
+        </form>
+        
+        ";        
+    }
+    else{
+        $module .= "<h2>Arquivo de dados '{$file}' não encontrado.</h2>";
+    }
+}
+?>
