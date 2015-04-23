ca7872ea03b9ab943ecf34cf6044e55cf2813cdd
Leandro <leandro_perrotta@hotmail.com>
2015-04-23 14:03:36 -0300

    * improvements to item shop

--- modules/store/add.php
+++ modules/store/add.php
@@ -3,6 +3,7 @@ class View
 {
 	//html fields 
 	private $_item_type,
+			$_item_category,
 			$_item_name,
 			$_item_id,
 			$_item_count,
@@ -29,29 +30,71 @@ class View
 			return false;
 		}
 		
+		$item = NULL;
+		
+		if(isset($_GET["edit"])){
+			$item = new \Framework\ItemShop();
+			
+    		if(!$item->load($_GET["edit"]))
+    		{
+    			$this->_message = \Core\Lang::Message(\Core\Lang::$e_Msgs->REPORT);
+    			\Core\Main::sendMessageBox(\Core\Lang::Message(\Core\Lang::$e_Msgs->ERROR), $this->_message);
+    			return false;
+    		}
+		}
+		
+		$isitem = $item ? (($item->getType() == \Framework\ItemShop::TYPE_ITEM) ? true : false) : false;
+		$iscallback = $item ? (($item->getType() == \Framework\ItemShop::TYPE_CALLBACK) ? true : false) : false;
+		$name = $item ? $item->getName() : "";
+		$itemid = $item ? $item->getParams()[\Framework\ItemShop::PARAM_ITEM_ID] : "";
+		$itemcount = $item ? $item->getParams()[\Framework\ItemShop::PARAM_ITEM_COUNT] : "";
+		$actionid = $item ? $item->getParams()[\Framework\ItemShop::PARAM_ITEM_ACTION_ID] : "";
+		$stackable = $item ? $item->getParams()[\Framework\ItemShop::PARAM_ITEM_STACKABLE] : false;
+		$description = $item ? $item->getDescription() : "";
+		$price = $item ? $item->getPrice() : "";
+		
+		$isequipment = $item ? (($item->getCategory() == \Framework\ItemShop::CATEGORY_EQUIPMENTS) ? true : false) : false;
+		$isweapon = $item ? (($item->getCategory() == \Framework\ItemShop::CATEGORY_WEAPONS) ? true : false) : false;
+		$isaddons = $item ? (($item->getCategory() == \Framework\ItemShop::CATEGORY_ADDONS) ? true : false) : false;
+		$isvaluable = $item ? (($item->getCategory() == \Framework\ItemShop::CATEGORY_VALUABLE) ? true : false) : false;
+		$isservice = $item ? (($item->getCategory() == \Framework\ItemShop::CATEGORY_SERVICES) ? true : false) : false;
+		
 		$this->_item_type = new \Framework\HTML\SelectBox();
 		$this->_item_type->SetName("item_type");	
-		$this->_item_type->AddOption("Item", \Framework\ItemShop::TYPE_ITEM);
-		$this->_item_type->AddOption("Callback", \Framework\ItemShop::TYPE_CALLBACK);
+		$this->_item_type->AddOption("Item", \Framework\ItemShop::TYPE_ITEM, $isitem);
+		$this->_item_type->AddOption("Callback", \Framework\ItemShop::TYPE_CALLBACK, $iscallback);
+		
+		$this->_item_category = new \Framework\HTML\SelectBox();
+		$this->_item_category->SetName("item_category");
+		$this->_item_category->SetSize(200);
+		$this->_item_category->AddOption("Equipamentos", \Framework\ItemShop::CATEGORY_EQUIPMENTS, $isequipment);
+		$this->_item_category->AddOption("Armas e Escudos", \Framework\ItemShop::CATEGORY_WEAPONS, $isweapon);		
+		$this->_item_category->AddOption("Addons e Outfits", \Framework\ItemShop::CATEGORY_ADDONS, $isaddons);		
+		$this->_item_category->AddOption("Valiosos e Diversos", \Framework\ItemShop::CATEGORY_VALUABLE, $isvaluable);		
+		$this->_item_category->AddOption("Serviços", \Framework\ItemShop::CATEGORY_SERVICES, $isservice);		
 		
 		$this->_item_name = new \Framework\HTML\Input();
 		$this->_item_name->SetName("item_name");
+		$this->_item_name->SetValue($name);
 		
 		$this->_item_id = new \Framework\HTML\Input();
-		$this->_item_id->SetName("item_id");	
+		$this->_item_id->SetName("item_id");
+		$this->_item_id->SetValue($itemid);
 		$this->_item_id->SetSize(10);	
 			
 		$this->_item_count = new \Framework\HTML\Input();
-		$this->_item_count->SetName("item_count");	
+		$this->_item_count->SetName("item_count");
+		$this->_item_count->SetValue($itemcount);
 		$this->_item_count->SetSize(10);		
 		
 		$this->_item_action_id = new \Framework\HTML\Input();
 		$this->_item_action_id->SetName("item_action_id");	
+		$this->_item_action_id->SetValue($actionid);	
 		$this->_item_action_id->SetSize(10);		
 		
 		$this->_item_is_stackable = new \Framework\HTML\Input();
 		$this->_item_is_stackable->IsCheackeable();
-		$this->_item_is_stackable->SetValue("true");
+		if($stackable) $this->_item_is_stackable->IsDefault();
 		$this->_item_is_stackable->SetName("item_is_stackable");		
 		
 		$this->_item_func = new \Framework\HTML\Input();
@@ -66,9 +109,11 @@ class View
 		$this->_item_description = new \Framework\HTML\Input();
 		$this->_item_description->IsTextArea(7, 50);
 		$this->_item_description->SetName("item_description");	
+		$this->_item_description->SetValue($description);	
 
 		$this->_item_price = new \Framework\HTML\Input();
 		$this->_item_price->SetName("item_price");		
+		$this->_item_price->SetValue($price);		
 		$this->_item_price->SetSize(10);				
 		
 		if($_POST)
@@ -107,7 +152,35 @@ class View
 	
 	function Post()
 	{
+	    $item = new \Framework\ItemShop();
+	    
+	    if(isset($_GET["edit"])){
+	        if(!$item->load($_GET["edit"]))
+	        {
+	            $this->_message = \Core\Lang::Message(\Core\Lang::$e_Msgs->REPORT);
+	            return false;
+	        }
+	    }
+	    
+	    if(isset($_GET["delete"])){
+	        if(!$item->load($_POST["item_id"]))
+	        {
+	            $this->_message = \Core\Lang::Message(\Core\Lang::$e_Msgs->REPORT);
+	            return false;
+	        }
+	        
+	        $item->delete();
+	        
+	        $ret["error"] = false;
+	        $ret["msg"] = "O item foi removido com sucesso!";
+	        
+	        echo json_encode($ret);
+	        
+	        die();	        
+	    }
+	    
 		$name = $this->_item_name->GetPost();
+		$category = $this->_item_category->GetPost();
 		$description = $this->_item_description->GetPost();
 		$price = $this->_item_price->GetPost();
 		$type = $this->_item_type->GetPost();
@@ -167,8 +240,8 @@ class View
 			return false;						
 		}			
 		
-		$item = new \Framework\ItemShop();
 		$item->setName($name);
+		$item->setCategory($category);
 		$item->setType($type);
 		$item->setDescription($description);
 		$item->setPrice($price);
@@ -196,8 +269,12 @@ class View
 		$item->setEnabled(true);
 		$item->save();
 		
-		$this->_message = "O item foi adicionado com sucesso.";
-		return false;				
+		if(!isset($_GET["edit"]))
+            $this->_message = "O item foi adicionado com sucesso.";
+        else
+            $this->_message = "O item foi editado com sucesso.";
+	    
+		return true;				
 	}
 	
 	function Draw()
@@ -245,10 +322,15 @@ class View
 				<p>
 					<label for='item_name'>Nome</label><br />
 					{$this->_item_name->Draw()}
+				</p>
+				
+				<p>
+					<label for='item_category'>Categoria</label><br />
+					{$this->_item_category->Draw()}
 				</p>	
 				
 				<p class='item_param'>
-					<label for='item_id'>Item ID</label><br />
+					<label for='item_id'>Item ID (consulte no items.xml)</label><br />
 					{$this->_item_id->Draw()}
 				</p>			
 
@@ -258,7 +340,7 @@ class View
 				</p>		
 
 				<p class='item_param'>
-					<label for='item_count'>Action ID (necessario para alguns itens)</label><br />
+					<label for='item_count'>Action ID (necessario para alguns itens especiais, por padrão deixe em branco)</label><br />
 					{$this->_item_action_id->Draw()}
 				</p>				
 				
@@ -287,7 +369,7 @@ class View
 				</p>
 							
 				<p>
-					<label for='item_price'>Preço (R$)</label><br />
+					<label for='item_price'>Preço (insira o valor sem virgulas, por ex: R$ 10,00 digite 1000)</label><br />
 					{$this->_item_price->Draw()}
 				</p>
 				
